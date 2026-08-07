2976d4ee4584314e72edc237f2627a5a2a4c09de
25ec01048-prog <25ec01048@iitbbs.ac.in>
2026-08-07 09:30:54 +0530

    added line to checkup_setup.py

--- check_setup.py
+++ check_setup.py
@@ -8,6 +8,7 @@ print("numpy :", np.__version__)
 print("pandas :", pd.__version__)
 print("matplotlib:", matplotlib.__version__)
 print("commit-1: 25EC01048")
+print("commit-2: 25EC01048")
 # a one-line smoke test of the plotting back-end
 plt.plot([0, 1, 2, 3], [0, 1, 4, 9], marker="o")
 plt.title("If you can see this window, the setup works")
