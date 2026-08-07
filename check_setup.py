b8cbd0906f5c484573f1a9fda35f8e2604673c1a
25ec01048-prog <25ec01048@iitbbs.ac.in>
2026-08-07 09:52:23 +0530

    changed plot label

--- check_setup.py
+++ check_setup.py
@@ -12,6 +12,6 @@ print("25EC01048 merge check complete.")
 # a one-line smoke test of the plotting back-end
 plt.plot([0, 1, 2, 3], [0, 1, 4, 9], marker="o")
 plt.title("If you can see this window, the setup works")
-plt.xlabel("x"); plt.ylabel("x squared")
+plt.xlabel("x"); plt.ylabel("y= x squared")
 plt.grid(True)
 plt.show()
